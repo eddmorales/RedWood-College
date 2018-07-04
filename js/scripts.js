da82c1d7c75749aefae5177e339882131244723a
Edgar Blancas Morales <eddmorales10@gmail.com>
2018-07-03 19:14:42 -0500

    Pagina casi terminada
    Falta contenido real

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,45 @@
 $(document).ready(function(){
 /*SCRIPT PARA AGREGAR CLASE ACTIVO*/
-    $("nav ul li a").click(function(e) {
+    $("nav ul li a").click(function() {
       $("nav ul li a.activo").removeClass('activo');
       var $this = $(this);
       if (!$this.hasClass('activo')) {
           $this.addClass('activo');
       }
-      e.preventDefault();
   	});
+  
 /*SCRIPT PARA ANIMACION DE MENU HAMBURGUESA*/
     var flag1 = false;
 	$(".hamburger").click(function(){
 		if(!flag1){
 			$(".hamburger").addClass("is-active");
+			$('nav').animate({
+              left: '0'
+          });
 			flag1 = true;
 		}
 		else{
 			$(".hamburger").removeClass("is-active");
+			 $('nav').animate({
+            left: '-100%'
+        	});
 			flag1 = false;
 		}
 	});
 /*SCRIPT PARA ANIMACION DE MENU DE NAVEGACION*/
 	var flag2 = false;
 	var scroll;
+
 	$(window).scroll(function(){
 		scroll = $(window).scrollTop();
 
-		if(scroll > 150){
+		if(scroll > 100 && window.innerWidth > 800){
 			if(!flag2){
-				$("header").css({"position": "fixed", "top": "0", "height": "100px", "z-index": "1000"});
-				$("#logo").css({"margin-top": "-5px", "width": "80px", "height": "80px"});
+				$("header").css({"position": "fixed", "top": "0", "height": "120px", "z-index": "1000"});
+				$("#logo").css({"margin-top": "-35px", "width": "170px", "height": "160px"});
 				$("header").css({"background-color": "#f8f8f8"});
-				$("nav").css({"margin-left": "9%"});
-				$("nav ul li").css({"margin-top": "15px"});
+				$("nav").css({"margin-left": "3%"});
+				$("nav ul li").css({"margin-top": "23px"});
 				$("header").css({"box-shadow":"0px 10px 25px 0px rgba(0,0,0,0.63)"});
 				$("header").css({"-moz-box-shadow":"0px 10px 25px 0px rgba(0,0,0,0.63)"});
 				$("header").css({"-webkit-box-shadow":"0px 10px 25px 0px rgba(0,0,0,0.63)"});
@@ -42,9 +49,9 @@ $(document).ready(function(){
 		else{
 			if(flag2) {
 				$("header").css({"position": "static"});
-				$("#logo").css({"margin-top": "50px", "width": "230px", "height": "230px"});
+				$("#logo").css({"margin-top": "0px", "width": "200px", "height": "200px"});
 				$("header").css({"background-color": "transparent"});
-				$("nav").css({"margin-left": "4%"});
+				$("nav").css({"margin-left": "10px"});
 				$("nav ul li").css({"margin-top": "0px"});
 				$("header").css({"box-shadow":"0px 10px 25px 0px rgba(0,0,0,0)"});
 				$("header").css({"-moz-box-shadow":"0px 10px 25px 0px rgba(0,0,0,0)"});
@@ -53,4 +60,18 @@ $(document).ready(function(){
 			}
 		}
 	});
+
+/*SCRIPTS PARA ANIMAR NUMEROS*/
+$('#profesores').animateNumber({number: 20}, 5000);
+$('#pae').animateNumber({number: 50}, 5000);
+$('#cursos').animateNumber({number: 35}, 5000);
+
+/*SCRIPT PARA CONOCER LA POSICIÓN DE LA VENTANA*/
+$(window).scroll(function(){
+	var scroll = $(window).scrollTop();
+	//console.log(scroll);
+});
+
+var anchoHeader = $('.redes-inicio').innerWidth();
+console.log(anchoHeader);
 });
